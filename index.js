77b375798094a73912f73f1f7470cdfb4146cd5b
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-12 13:55:30 -0400

    Parado en error al intentar ejecutar updateCourse desde graphiql

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql')
+
+
+// data
+const { courses } = require('./data.json');
+
+const schema = buildSchema(`
+  type Query {
+    course(id: Int!): Course
+    courses(topic: String): [Course]
+  }
+
+  type Mutation {
+    updateCourseTopic(id: Int!, topic: String!): Course
+  }
+
+  type Course {
+    id: Int
+    title: String
+    description: String
+    author: String
+    topic: String
+    url: String
+  }
+`);
+
+
+let getCourse = (args) => {
+  let id = args.id;
+  return courses.filter(course => {
+    return course.id === id;
+  })[0]
+}
+
+let getCourses = (args) => {
+  if(args.topic) {
+    let topic = args.topic;
+    return courses.filter(course => course.topic === topic);
+  }
+  else {
+    return courses;
+  }
+}
+
+let updateCourseTopic = ({id, topic}) => {
+  courses.map(course => {
+    if(course.id === id) {
+      course.topic = topic;
+      return course;
+    }
+  })
+  return course.filter(course => course.id === id)[0];
+}
+
+const root = {
+  course: getCourse,
+  courses: getCourses,
+  updateCourseTopic: updateCourseTopic
+}
+
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(3000, () => {
+  console.log('server on port 3000');
+});
+
+
